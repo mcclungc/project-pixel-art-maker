fc17e09116e3f0b8ba6fe51aae057b53d2a6aa18
Connie McClung <connie_mcclung@comcast.net>
2022-07-30 10:24:35 -0400

    revised per project review

--- designs.js
+++ designs.js
@@ -1,48 +1,42 @@
-// Connie McClung - Pixel Art Project
-// 07/29/2022
+// Connie McClung - Pixel Art Project Revised
+// 07/30/2022
 
-// Select color input
+// Select color input TO DO: update gridColor value upon change
 const gridColor = document.querySelector("#colorPicker");
 
 // Select size input
 const gridHeight = document.querySelector("#inputHeight");
-
 const gridWidth = document.querySelector("#inputWidth");
 
-// When size is submitted by the user, call makeGrid()
+// When size is submitted by the user, call makeGrid
 const setupForm = document.querySelector("#sizePicker");
-
 setupForm.addEventListener("submit", makeGrid);
+
+//add a single event listener for click events in table
+let canvas = document.querySelector("#pixelCanvas");
+canvas.addEventListener("click", changeCell);
+
 
 function makeGrid(event) {
 	event.preventDefault();
+	let canvas = document.querySelector("#pixelCanvas");
+	makeTable(canvas);
+	console.log(canvas);
+}
 
-	const rows = gridHeight.value;
-	const cols = gridWidth.value;
-	const cellColor = gridColor.value;
-	const canvas = document.querySelector("#pixelCanvas");
 
-	// make table with submitted height and width dimensions
-	for (let r = 1; r <= rows; r++) {
-		let newTR = document.createElement("tr");
-		canvas.appendChild(newTR);
-		for (let c = 1; c <= cols; c++) {
-			let newTD = document.createElement("td");
-			newTR.appendChild(newTD);
-		}
-	}
-	//use a single event listener to respond to click events in table cells
-	canvas.addEventListener("click", changeCell);
-	
-	//toggle to add/remove user-defined color when cell is clicked
-	function changeCell(event) {
+//toggle to add/remove user-defined color when cell is clicked
+function changeCell(event) {
+	// only respond if event target is a table cell
+	if (event.target.classList.contains("newTD")) {
 		// if class "clicked" is present, cell is colored--change back and remove the class
 		if (event.target.classList.contains("clicked")) {
 			event.target.style.backgroundColor = "white";
 			event.target.classList.remove("clicked");
 		}
-		// if class "clicked" is not present, set color and add the class
+		// if class "clicked" is not present, set color to current #colorPicker value and add the class
 		else {
+			let cellColor = gridColor.value;
 			event.target.style.backgroundColor = cellColor;
 			event.target.classList.add("clicked");
 		}
@@ -50,3 +44,23 @@ function makeGrid(event) {
 }
 
 
+// make table with submitted height and width dimensions
+function makeTable(canvas) {
+	// clear existing table 
+	if (canvas.classList.contains("has_table")) {
+		canvas.innerHTML = "";
+		canvas.classList.remove("has_table");
+	}
+	let rows = gridHeight.value;
+	let cols = gridWidth.value;
+	for (let r = 1; r <= rows; r++) {
+		let newTR = document.createElement("tr");
+		canvas.appendChild(newTR);
+		for (let c = 1; c <= cols; c++) {
+			let newTD = document.createElement("td");
+			newTD.classList.add("newTD");
+			newTR.appendChild(newTD);
+		}
+		canvas.classList.add("has_table")
+	}
+}
